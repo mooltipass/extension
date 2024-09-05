f0e0fabc897657af540442874a9e8d6e3221519d
limpkin <limpkin@limpkin.fr>
2024-09-05 21:31:09 +0200

    zip making script: use the base of the previously discarded script

--- build_mv2.sh
+++ build_mv2.sh
@@ -12,11 +12,30 @@ if [ ! -d "$SOURCE_DIR" ]; then
   exit 1
 fi
 
-# Create the build directory if it does not exist
-mkdir -p "$BUILD_DIR"
+# Remove existing contents of the build directory
+if [ -d "$BUILD_DIR" ]; then
+  rm -rf "$BUILD_DIR"/*
+  echo "Cleaned up existing contents in $BUILD_DIR"
+else
+  mkdir -p "$BUILD_DIR"
+fi
+
+# Inject Scripts
+cp "${SOURCE_DIR}/mooltipass-content.js" "${BUILD_DIR}/mooltipass-content.js"
+cp "${SOURCE_DIR}/mooltipass-content.css" "${BUILD_DIR}/mooltipass-content.css"
+cp "${SOURCE_DIR}/manifest.json" "${BUILD_DIR}/"
 
 # Copy the contents of the extension_mv2 folder into the build folder
-cp -r "$SOURCE_DIR"/* "$BUILD_DIR"/
+for ext_dir in vendor popups css options background images icons ui content_scripts _locales first_run_documents shared_scripts; do
+	cp -Rf "${SOURCE_DIR}/${ext_dir}" "${BUILD_DIR}/"
+done
+
+if [ "${ENABLE_EMULTATION_MODE}" != '0' ] && [ -f "${BUILD_DIR}/vendor/mooltipass/device.js" ]; then
+	if ! sed -i 's/mooltipass.device.emulation_mode = false/mooltipass.device.emulation_mode = true/' \
+		 "${BUILD_DIR}/vendor/mooltipass/device.js"; then
+		echo "[ERROR] Cannot set emulation mode" 1>&2
+	fi
+fi
 
 # Check if an argument is provided
 if [ -n "$1" ]; then
